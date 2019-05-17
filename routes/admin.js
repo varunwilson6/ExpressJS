b8dd2152bb6f08ad3dc7558c3d4158ee312386a6
Varun Wilson <varunwilson6@gmail.com>
2019-05-17 15:25:06 +0530

    expressJS upto dynamic rendering

---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {Pagetitle:"Add Product"});
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title:req.body.title,
+                  price:req.body.price,
+                  description:req.body.description,
+                  Image_URL:req.body.Image_URL});
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
